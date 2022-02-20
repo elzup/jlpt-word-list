42698c6fce49f8cf08f81123f801a9c51b352e42
elzup <guild0105@gmail.com>
2022-02-20 14:15:28 +0900

    add merge script

--- scripts/merge.js
+++ scripts/merge.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 'use strict'
 
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 
-readFileSync('')
+const concat = (a, init = []) => a.reduce((p, c) => p.concat(c), init)
+const range = (n) => [...Array(n).keys()]
+const main = async () => {
+  const files = range(5)
+    .map((v) => v + 1)
+    .map((i) => readFileSync(`src/n${i}.csv`, 'utf-8').trim().split('\n'))
+  const head = files[0][0]
+  files.forEach((f) => {
+    f.shift()
+  })
+  const result = concat(files, [head])
+  const text = result.join('\n')
 
-const main = async () => {}
+  writeFileSync('out/all.csv', text, 'utf-8')
+}
 
 main()
