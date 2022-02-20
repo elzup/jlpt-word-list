dd28cf79f202d5263d8bcf40d5cf963589833f5e
elzup <guild0105@gmail.com>
2022-02-20 14:19:30 +0900

    add compact options

--- scripts/merge.js
+++ scripts/merge.js
@@ -5,18 +5,30 @@ const { readFileSync, writeFileSync } = require('fs')
 
 const concat = (a, init = []) => a.reduce((p, c) => p.concat(c), init)
 const range = (n) => [...Array(n).keys()]
-const main = async () => {
-  const files = range(5)
+
+const main = async (outFile = 'out/all.csv', toN = 5, min = false) => {
+  const files = range(toN)
     .map((v) => v + 1)
     .map((i) => readFileSync(`src/n${i}.csv`, 'utf-8').trim().split('\n'))
   const head = files[0][0]
   files.forEach((f) => {
     f.shift()
   })
-  const result = concat(files, [head])
+  const result = concat(files, [head]).map((line) => {
+    if (min) {
+      const [expression, reading, _meaning, _tags] = line.split(',')
+      return [expression, reading].join(',')
+    }
+    return line
+  })
+
   const text = result.join('\n')
 
-  writeFileSync('out/all.csv', text, 'utf-8')
+  writeFileSync(outFile, text, 'utf-8')
 }
 
-main()
+// full
+// main('out/all.csv', 5, false)
+
+// min
+main('out/all.min.csv', 5, true)
